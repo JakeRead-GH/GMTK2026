92a2b7724b89fbfe1108fd8c725fb9c67a48724d
James Araiche <james.araiche@gmail.com>
2026-07-24 17:30:42 -0400

    Fix Keyboard Functionality On Dev

---
+++ Assets/Scripts/SoundFXManager.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class SoundFXManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public static SoundFXManager instance;
+    
+
+}
